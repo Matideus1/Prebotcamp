536b232b3d70a6a0d14137bc8cbb40cc2869757a
Matideus1 <108019153+Matideus1@users.noreply.github.com>
2022-09-09 11:55:35 -0300

    confirmar

---
+++ predecirCodigoJS/niñomontaña.js
@@ -0,0 +1,19 @@
+
+var Alturaniño = 56;
+
+function muestrasielniñopuedesubiralamontañarusa(alturaniño){
+
+
+if(alturaniño  > 56 ){
+
+    console.log("!subete,chico!")
+}else{
+
+    console.log("lo siento,chico.tal vez el proximo año")
+}
+
+
+}
+
+
+
